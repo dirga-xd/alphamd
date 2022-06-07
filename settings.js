54ca6350476f73dc6f30aaed683b872f07d3ca07
RendyXD <97732589+RendyXD@users.noreply.github.com>
2022-06-08 00:54:16 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -14,8 +14,8 @@ global.APIKeys = {
 // setting 
 global.autoread = false // auto read pesan / message
 //global.autorecording = true //status auto merekam ( auto record )
-//global.autoketik = false //status auto mengetik (auto typing)
-//global.available = false //status online (online)
+//global.autoketik = true //status auto mengetik (auto typing)
+//global.available = true //status online (online)
 
 // Other
 global.botname = "XL-XD BOT" //namabot kalian
